d4b3d89da8950fee4c4bcb37bb2abc7566b24eb0
Seraf <julien.syx@gmail.com>
2013-07-06 18:01:37 +0200

    Changing the way the module is loaded

--- lang/fr/vera.rs
+++ lang/fr/vera.rs
@@ -5,12 +5,6 @@
 @ quelle temperature fait il dans * ?
 
 > object gettemperature python
-    import sys, os, inspect
-    cmd_subfolder = os.path.realpath(os.path.abspath(os.path.join(os.path.split( \
-        inspect.getfile(inspect.currentframe()))[0],os.path.normpath("Plugins/Vera/modules/"))))
-    if cmd_subfolder not in sys.path:
-        sys.path.insert(0, cmd_subfolder)
-
-    from vera import Vera
+    from Vera.modules.vera import Vera
     return Vera().getTemperature(args)
 < object
